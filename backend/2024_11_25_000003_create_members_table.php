a874e5d8915f59d86f640e6d2a6785376d90ff27
Marc <marc.orgs@schuengelschwarz.de>
2025-04-26 03:45:45 +0200

    Update 2024_11_25_000003_create_members_table.php

--- backend/2024_11_25_000003_create_members_table.php
+++ backend/2024_11_25_000003_create_members_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('tour_id')->nullable(); // Ref tours.id
+ 
 
             $table->foreign('tour_id')
                   ->references('id')
