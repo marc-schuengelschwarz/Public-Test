f9e456acde269777f065bd6f90a32f8122fe191d
Marc <marc.orgs@schuengelschwarz.de>
2025-04-26 03:13:08 +0200

    Update 2024_11_17_151129_create_persons_table.php

--- backend/2024_11_17_151129_create_persons_table.php
+++ backend/2024_11_17_151129_create_persons_table.php
@@ -20,24 +20,7 @@ return new class extends Migration
             $table->string('titel')->nullable();
             $table->string('job_bezeichnung')->nullable();
             $table->unsignedBigInteger('institution')->nullable(); // Ref institutions.id
-            $table->string('land')->default('Deutschland')->nullable();
-            $table->string('plz')->nullable();
-            $table->string('ort')->nullable();
-            $table->string('strasse')->nullable();
-            $table->string('hausnummer')->nullable();
-            $table->string('google_maps')->nullable();
-            $table->string('homepage')->nullable();
-            $table->string('telefon')->nullable();
-            $table->string('mobil')->nullable();
-            $table->string('mail')->nullable();
-            $table->text('notiz')->nullable();
-            $table->string('status')->default('aktiv')->nullable();
-            $table->timestamps();
-
-            $table->foreign('institution')
-                  ->references('id')
-                  ->on('institutions')
-                  ->onDelete('set null');
+ 
         });                
     }
 
