fe24a2ace621d202e37508f8880040d6dd6dc480
Marc <marc.orgs@schuengelschwarz.de>
2025-04-26 03:38:08 +0200

    Update 2024_11_17_151129_create_persons_table.php

--- backend/2024_11_17_151129_create_persons_table.php
+++ backend/2024_11_17_151129_create_persons_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
             $table->string('vorname');
-            $table->string('nachname');
-            $table->string('titel')->nullable();
  
         });                
     }
